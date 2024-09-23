f4a3dd4b6c9d236015fcb501110de2526d1a05c9
Vikas Pandey <svikas.pandey@gmail.com>
2024-09-23 14:18:08 +0530

    fix guard_path

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,19 @@ use Nextvikas\Authenticator\Middleware\AuthChecker;
 Route::middleware([AuthChecker::class])->group(function () {
 
      // Retrieve the guard name from the configuration.
-     $login_guard_path = Config::get('authenticator.login_guard_path');
+     $guard_path = Config::get('authenticator.guard_path');
 
     // Route for displaying the two-step verification page.
-    Route::get($login_guard_path.'/verify-two-step', [AuthenticatorController::class, 'verify_two_step'])
+    Route::get($guard_path.'/verify-two-step', [AuthenticatorController::class, 'verify_two_step'])
          ->name('authenticator.verify'); // Name the route for easy reference.
 
     // Route for processing the two-step verification form submission.
-    Route::post($login_guard_path.'/verify-two-step', [AuthenticatorController::class, 'verify_two_step_process']);
+    Route::post($guard_path.'/verify-two-step', [AuthenticatorController::class, 'verify_two_step_process']);
     
     // Route for displaying the QR code scanning page for two-step verification.
-    Route::get($login_guard_path.'/scan-two-step', [AuthenticatorController::class, 'scan_two_step'])
+    Route::get($guard_path.'/scan-two-step', [AuthenticatorController::class, 'scan_two_step'])
          ->name('authenticator.scan'); // Name the route for easy reference.
 
     // Route for processing the QR code scan form submission.
-    Route::post($login_guard_path.'/scan-two-step', [AuthenticatorController::class, 'scan_two_step_process']);
+    Route::post($guard_path.'/scan-two-step', [AuthenticatorController::class, 'scan_two_step_process']);
 });
